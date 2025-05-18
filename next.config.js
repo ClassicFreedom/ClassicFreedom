a22e222eced277bd8de34d1c402e707d967aeaa6
ClassicFreedom <gordyxdxd@gmail.com>
2025-05-18 14:36:32 -0300

    Update Next.js configuration and dependencies

--- next.config.js
+++ next.config.js
@@ -1,1 +1,14 @@
- 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: false,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
+}
+
+module.exports = nextConfig 
